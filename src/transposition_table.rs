af1b7dace916e3f8a491d9ff20804c6b92a280f1
Joachim Nebel <Joachim.E.Nebel@gmail.com>
2022-10-28 12:48:55 +0200

    Recording and probing TT

--- src/transposition_table.rs
+++ src/transposition_table.rs
@@ -1,7 +1,6 @@
 use super::*;
 
 const TT_SIZE: usize = 0x16_00000 / 18; // (byte size of TT) / (Size of TT entry)
-const NULL_TT_ENTRY: TranspositionTableEntry = TranspositionTableEntry{key: 0, depth: 0, flag: HashFlag::Alpha, score: 0, best: NULL_MOVE};
 
 #[derive(Copy, Clone)]
 pub enum HashFlag {
@@ -11,12 +10,15 @@ pub enum HashFlag {
 }
 
 #[derive(Copy, Clone)]
-pub struct TranspositionTableEntry {
-    key: u64,
-    depth: u8,
-    flag: HashFlag,
-    score: i32,
-    best: Move
+pub enum TranspositionTableEntry {
+    Record {
+        hash: u64,
+        depth: u8,
+        flag: HashFlag,
+        score: i32,
+        best: Move
+    },
+    Empty
 }
 
 pub struct TranspositionTable {
@@ -24,17 +26,31 @@ pub struct TranspositionTable {
 }
 
 impl TranspositionTableEntry {
-    pub fn new(key: u64, depth: u8, flag: HashFlag, score: i32, best: Move) -> Self {
-        Self { key: key, depth: depth, flag: flag, score: score, best: best }
+    pub fn new(hash: u64, depth: u8, flag: HashFlag, score: i32, best: Move) -> Self {
+        Self::Record { hash: hash, depth: depth, flag: flag, score: score, best: best }
     }
 }
 
 impl TranspositionTable {
     pub fn new() -> Self {
-        Self{ table: vec![NULL_TT_ENTRY; TT_SIZE] }
+        Self{table: vec![TranspositionTableEntry::Empty; TT_SIZE]}
     }
 
-    pub fn record_position(&mut self) {
+    pub fn record_position(&mut self, hash: u64, score: i32, depth: u8, flag: HashFlag, best: Move) {
+        self.table[(hash % TT_SIZE as u64) as usize] = TranspositionTableEntry::new(hash, depth, flag, score, best)
+    }
 
+    pub fn probe(&mut self, zhash: u64, depth: u8, alpha: i32, beta: i32) -> &TranspositionTableEntry {
+        let entry = &self.table[(zhash % TT_SIZE as u64) as usize];
+
+        match entry {
+            TranspositionTableEntry::Record { hash, depth, flag, score, best } => if zhash == *hash {
+                    return entry;
+                }
+                else {
+                    &TranspositionTableEntry::Empty
+                },
+            TranspositionTableEntry::Empty => &TranspositionTableEntry::Empty,
+        }
     }
 }
